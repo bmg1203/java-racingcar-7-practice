303722ab3f039cc508ddf548fb939b94782faf1a
chan <ckstj8027@naver.com>
2024-10-27 21:34:22 +0900

    refactor(domain): parseLong으로바꿔 정수범위 체크해 에러메시지 다각화

--- src/main/java/racingcar/domian/TryNumber.java
+++ src/main/java/racingcar/domian/TryNumber.java
@@ -4,6 +4,7 @@ public class TryNumber {
 
     private int tryNumber;
 
+
     private TryNumber(int tryNumber) {
         this.tryNumber = tryNumber;
     }
@@ -11,19 +12,40 @@ public class TryNumber {
 
     public static TryNumber createTryNumber(String tryNumber) {
         int convertedNumber = convertToInteger(tryNumber);
+        isPositiveNumber(convertedNumber);
         return new TryNumber(convertedNumber);
     }
 
+
     private static int convertToInteger(String tryNumber) {
         try {
-            return Integer.parseInt(tryNumber);
+            long parseLong = Long.parseLong(tryNumber);
+            return validateMaxRange(parseLong);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자를 입력해주세요");
         }
+
     }
+
+
+    private static int validateMaxRange(long parseLong) {
+        if (parseLong > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("시도횟수의범위를 확인해주세요. " + "[" + "1 ~ " + Integer.MAX_VALUE + "]");
+        }
+        return (int) parseLong;
+    }
+
 
     public int getTryNumber() {
         return tryNumber;
+    }
+
+
+    private static boolean isPositiveNumber(int tryNumber) {
+        if (tryNumber > 0) {
+            return true;
+        }
+        throw new IllegalArgumentException("시도횟수는 양수를 입력해주세요.");
     }
 
 
