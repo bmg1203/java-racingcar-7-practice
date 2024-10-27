a24a898cc837cb3e586f3139084bf90dec9a013e
chan <ckstj8027@naver.com>
2024-10-27 20:27:43 +0900

    feat(application): application 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+
+import racingcar.carRacingManager.CarRacingManager;
+import racingcar.controller.Controller;
+import racingcar.repository.CarRepository;
+import racingcar.service.Service;
+import racingcar.service.ServiceImpl;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+
+        CarRepository carRepository = new CarRepository();
+        CarRacingManager carRacingManager = new CarRacingManager(carRepository);
+        Service service = new ServiceImpl(carRepository, carRacingManager);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Controller controller = new Controller(service, carRepository, inputView, outputView);
+        controller.play();
+
     }
+
 }
