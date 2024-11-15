1d60e00507f0fe8fec756076ffa2d507dac894f6
bmg1203 <bmg1203@naver.com>
2024-11-15 23:38:37 +0900

    [feat]: InputView 자동차 이름 입력 기능에 입력 프롬프트 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,12 +2,14 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import racingcar.constants.InputPrompts;
 import racingcar.utils.Split;
 import racingcar.validator.InputValidator;
 
 public class InputView {
 
     public List<String> inputCarNames() {
+        System.out.println(InputPrompts.CAR_NAME_INPUT.getInputPrompt());
         String input = Console.readLine();
         List<String> names = Split.commaSplit(input);
 
