e6f3c500b8238540ec6ce30b44d4b0af2a16cb34
bmg1203 <bmg1203@naver.com>
2024-11-16 04:30:13 +0900

    [refactor]: Split 입력값 공백 없애기 제거
    - 다른 곳에서 공백을 제거하므로 Split에서는 분리만하도록 수정합니다.

--- src/main/java/racingcar/utils/Split.java
+++ src/main/java/racingcar/utils/Split.java
@@ -6,9 +6,7 @@ public class Split {
 
     private static final String COMMA = ",";
 
-    //쉼표기준 자르기
     public static List<String> commaSplit(String input) {
-        input = input.replaceAll(" ", "");
         return List.of(input.split(COMMA));
     }
 }
