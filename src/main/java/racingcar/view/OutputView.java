379b2e6156f2ff889470379e520a78431487213f
bmg1203 <bmg1203@naver.com>
2024-11-16 00:14:34 +0900

    [feat]: OutputView 출력 기능 추가
    - 시도 회수당 결과 출력 기능
    - 최종 우승자 출력 기능

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.constants.OutputPrompts;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+public class OutputView {
+
+    private static final String DISTANCE_STRING = "-";
+
+    public static void tryResultPrint(Cars cars) {
+        for (Car car : cars.getCars().values()) {
+            System.out.printf(OutputPrompts.TRY_RESULT.getOutputPrompt(),
+                    car.getName(), DISTANCE_STRING.repeat(car.getDistance()));
+        }
+    }
+
+    public static void winnersPrint(List<String> winners) {
+        System.out.printf(OutputPrompts.FINAL_RESULT.getOutputPrompt(),
+                String.join(", ", winners));
+    }
+}
