a5f7e2326cc5b376cbc04a6b554b1cd5a50f6897
bmg1203 <bmg1203@naver.com>
2024-11-16 04:44:26 +0900

    [test]: ApplicationTest 결과 관련 테스트 추가
    - 시도 한번 우승자 한명
    - 시도 한번 우승자 여러명
    - 시도 여러번 우승자 한명
    - 시도 여러번 우승자 여러명

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -52,6 +52,67 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    //결과 관련 테스트
+    @Test
+    void 시도_한번_우승자_한명_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : ", "woni : -", "최종 우승자 : woni");
+                },
+                STOP, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 시도_한번_우승자_여러명_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 시도_여러번_우승자_한명_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "5");
+                    assertThat(output()).contains("pobi : -", "woni : -", "jun : ");
+                    assertThat(output()).contains("pobi : --", "woni : -", "jun : ");
+                    assertThat(output()).contains("pobi : --", "woni : --", "jun : -");
+                    assertThat(output()).contains("pobi : ---", "woni : ---", "jun : -");
+                    assertThat(output()).contains("pobi : ----", "woni : ---", "jun : -");
+                    assertThat(output()).contains("최종 우승자 : pobi");
+
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP,
+                MOVING_FORWARD, STOP, STOP,
+                STOP, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, STOP,
+                MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @Test
+    void 시도_여러번_우승자_여러명_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni,jun", "3");
+                    assertThat(output()).contains("pobi : -", "woni : -", "jun : -");
+                    assertThat(output()).contains("pobi : --", "woni : -", "jun : -");
+                    assertThat(output()).contains("pobi : --", "woni : -", "jun : --");
+                    assertThat(output()).contains("최종 우승자 : pobi, jun");
+
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, STOP, STOP,
+                STOP, STOP, MOVING_FORWARD
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
