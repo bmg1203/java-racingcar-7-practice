ed75ce71e1a6ac6b2b04d70860709e7271f837d7
bmg1203 <bmg1203@naver.com>
2024-11-16 01:07:39 +0900

    [style]: OutputView static 삭제

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,14 +9,14 @@ public class OutputView {
 
     private static final String DISTANCE_STRING = "-";
 
-    public static void tryResultPrint(Cars cars) {
+    public void tryResultPrint(Cars cars) {
         for (Car car : cars.getCars().values()) {
             System.out.printf(OutputPrompts.TRY_RESULT.getOutputPrompt(),
                     car.getName(), DISTANCE_STRING.repeat(car.getDistance()));
         }
     }
 
-    public static void winnersPrint(List<String> winners) {
+    public void winnersPrint(List<String> winners) {
         System.out.printf(OutputPrompts.FINAL_RESULT.getOutputPrompt(),
                 String.join(", ", winners));
     }
